16ba5ae85c99fc412206781aa5359b6f191fc028
c0c24006c7 <c0c24006c7@edu.teu.ac.jp>
2025-06-10 16:39:31 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -10,23 +10,36 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img_flipped = pg.transform.flip(bg_img, True, False)
-    kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    bg_img2 = pg.transform.flip(bg_img,True,False)
+    kk_img=pg.image.load("fig/3.png")
+    kk_img=pg.transform.flip(kk_img, True,False)
+    kk_rct=kk_img.get_rect()
+    kk_rct.center=300,200
     tmr = 0
+    tobu=0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
-        x = -tmr % 3200
-        screen.blit(bg_img, [x , 0])
-        screen.blit(bg_img,[x-1600,0])
-        screen.blit(bg_img, [x - 3200, 0])
-
-        screen.blit(kk_img, [300,200])
+        key_lst=pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0,-1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0,1)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1,0)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1,0)
+        screen.blit(bg_img, [tobu, 0])
+        screen.blit(bg_img2, [tobu+1600, 0])
+        screen.blit(bg_img, [tobu+3200, 0])
+        screen.blit(kk_img,kk_rct)
         pg.display.update()
-        tmr += 1        
+        print(tobu)
+        if tobu<=-3199:
+            tobu=0
+        tmr += 1  
+        tobu -=5   
         clock.tick(200)
 
 
